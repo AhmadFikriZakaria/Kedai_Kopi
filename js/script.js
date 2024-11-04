af46dab665fdfc34de896affc1611bac6993cc0f
ahmadfikrizakaria <ahmad.zakaria@mhs.unsoed.ac.id>
2024-11-05 06:27:04 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav')
 
 // Ketika hamburger menu di klik
@@ -7,11 +7,35 @@ document.querySelector('#hamburger-menu').onclick = () => {
 };
 
 
-// Klik diluar sidebar untuk menghilangkan nav
-const hamburger =  document.querySelector('#hamburger-menu')
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+
+// Ketika elemen id search-button di klik
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus(); //  Fokuskan pada input search   
+    e.preventDefault(); // Mencegah aksi default
+
+
+};
+
+
+
+
+// Klik diluar elemen
+const hm =  document.querySelector('#hamburger-menu');
+const sb =  document.querySelector('#search-button');
+
 
 document.addEventListener('click', function(e) {
-    if (!hamburger.contains(e.target) &&  !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) &&  !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+    if (!sb.contains(e.target) &&  !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+    if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+      }
 });
